96c8b188cecbc4e2ba8ba3a00093f2c331649898
adedamolabasit <adedamolabasit09@gmail.com>
2024-05-17 17:40:38 +0100

    feat: project pdates

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,95 +1,7 @@
 import React, { useState } from "react";
 import Fade from "react-reveal/Fade";
+import { projects } from "./Project";
 
-const projects = [
-  {
-    title: "BlockOps Network",
-    description: "A brief description of project one.",
-    imageUrl: "https://via.placeholder.com/600x400?text=BlockOps Network",
-    projectUrl: "https://blockops.network/",
-    details: {
-      role: "As the lead developer, my responsibilities included designing and implementing the automated provisioning system for blockchain nodes. I collaborated with the team to define requirements and ensure seamless integration with existing infrastructure sioning system for blockchain nodes. I collaborated with the team to define requirements and ensure seamless integration with existing infrastructure.",
-      workedOn:
-        "I leveraged Docker containers and Kubernetes orchestration to streamline the deployment process of blockchain nodes. Additionally, I developed custom scripts using Python to automate configuration and provisioning tasks, reducing manual intervention by 80%.",
-      achievements:
-        "By implementing the automated provisioning system, we reduced node deployment time from hours to minutes, resulting in a 60% increase in operational efficiency. Our solution also decreased infrastructure costs by optimizing resource utilization.",
-      // Add more details as needed
-    },
-  },
-  {
-    title: "Stable Borrow",
-    description: "A brief description of project two.",
-    imageUrl: "https://via.placeholder.com/600x400?text=Stable Borrow",
-    projectUrl: "http://example.com/project2",
-    details: {
-      role: "My role in the project.",
-      workedOn: "What I worked on in the project.",
-      achievements: "Achievements of the project.",
-      // Add more details as needed
-    },
-  },
-  {
-    title: "Nautilus Technologies Website",
-    description: "A brief description of project two.",
-    imageUrl: "https://via.placeholder.com/600x400?text=Nautilus Technologies",
-    projectUrl: "http://example.com/project2",
-    details: {
-      role: "My role in the project.",
-      workedOn: "What I worked on in the project.",
-      achievements: "Achievements of the project.",
-      // Add more details as needed
-    },
-  },
-  {
-    title: "Symphy",
-    description: "A brief description of project two.",
-    imageUrl: "https://via.placeholder.com/600x400?text=Symphy",
-    projectUrl: "http://example.com/project2",
-    details: {
-      role: "My role in the project.",
-      workedOn: "What I worked on in the project.",
-      achievements: "Achievements of the project.",
-      // Add more details as needed
-    },
-  },
-  {
-    title: "Geolandmark",
-    description: "A brief description of project two.",
-    imageUrl: "https://via.placeholder.com/600x400?text=Geolandmark",
-    projectUrl: "http://example.com/project2",
-    details: {
-      Role: "My role in the project.",
-      workedOn: "What I worked on in the project.",
-      achievements: "Achievements of the project.",
-      // Add more details as needed
-    },
-  },
-  {
-    title: "BulkMailer",
-    description: "A brief description of project two.",
-    imageUrl: "https://via.placeholder.com/600x400?text=BulkMailer",
-    projectUrl: "http://example.com/project2",
-    details: {
-      role: "My role in the project.",
-      workedOn: "What I worked on in the project.",
-      achievements: "Achievements of the project.",
-      // Add more details as needed
-    },
-  },
-  {
-    title: "BulkMailer",
-    description: "A brief description of project two.",
-    imageUrl: "https://via.placeholder.com/600x400?text=BulkMailer",
-    projectUrl: "http://example.com/project2",
-    details: {
-      role: "My role in the project.",
-      workedOn: "What I worked on in the project.",
-      achievements: "Achievements of the project.",
-      // Add more details as needed
-    },
-  },
-  // Add more projects as needed
-];
 
 const ITEMS_PER_PAGE = 3; // Number of projects per page
 
@@ -231,6 +143,7 @@ const Portfolio = () => {
               )}
               {/* Add more toggles for additional details */}
             </div>
+            
 
             <button
               onClick={closeModal}
